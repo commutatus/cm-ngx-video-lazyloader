6084d2d08f63c78c20ae9a5e07b6c058233b5201
ramesh <ramesh.nagaraj21@gmail.com>
2021-04-09 15:50:16 +0530

    fix: Updated demo

--- projects/cm-ngx-video-lazyloader-demo/src/app/app.component.ts
+++ projects/cm-ngx-video-lazyloader-demo/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { CmNgxVideoLazyloaderService } from './../../../cm-ngx-video-lazyloader/src/lib/cm-ngx-video-lazyloader.service';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'cm-ngx-video-lazyloader-demo';
+
+  constructor(private lazyLoaderService: CmNgxVideoLazyloaderService) {
+  }
+
+  ngAfterViewInit() {
+    this.lazyLoaderService.lazyLoadVideos();
+  }
 }
