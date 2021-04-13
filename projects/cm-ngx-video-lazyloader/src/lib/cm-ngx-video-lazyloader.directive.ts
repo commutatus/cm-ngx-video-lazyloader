4ceed35800d804504f4903dc2b019067ebe98e73
ramesh <ramesh.nagaraj21@gmail.com>
2021-04-13 13:23:10 +0530

    feat: Test project with new directory and optimisations

---
+++ projects/cm-ngx-video-lazyloader/src/lib/cm-ngx-video-lazyloader.directive.ts
@@ -0,0 +1,43 @@
+import { Directive, ElementRef, OnDestroy, OnInit, Input } from '@angular/core';
+
+@Directive({
+  selector: '[libLazyloadVideo]'
+})
+export class CmNgxVideoLazyloaderDirective implements OnInit, OnDestroy {
+
+  @Input() lazyLoadPoster = false;
+  lazyVideoObserver: IntersectionObserver;
+  videoElement: Element;
+
+  constructor(private elRef: ElementRef) {
+  }
+
+  ngOnInit() {
+    this.videoElement = this.elRef.nativeElement;
+    if ('IntersectionObserver' in window) {
+      this.lazyVideoObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((video: any) => {
+          if (video.isIntersecting) {
+            if (this.lazyLoadPoster && video.target && video.target.dataset) {
+              video.target.poster = video.target.dataset.poster;
+            }
+            const videoHtmlCollection = [...video.target.children];
+            videoHtmlCollection.forEach((source: HTMLVideoElement) => {
+              if (typeof source.nodeName === 'string' && source.nodeName === 'SOURCE') {
+                source.src = source.dataset.src;
+              }
+            });
+
+            video.target.load();
+            this.lazyVideoObserver.unobserve(video.target);
+          }
+        });
+      });
+      this.lazyVideoObserver.observe(this.videoElement);
+    }
+  }
+
+  ngOnDestroy() {
+    this.lazyVideoObserver.unobserve(this.videoElement);
+  }
+}
